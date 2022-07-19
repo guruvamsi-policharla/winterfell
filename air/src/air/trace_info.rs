e71f52c11be0d198652bc048a1242a33801353d1
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2022-07-19 16:44:43 -0700

    Increased tracetable limits

--- air/src/air/trace_info.rs
+++ air/src/air/trace_info.rs
@@ -33,7 +33,7 @@ impl TraceInfo {
     /// Smallest allowed execution trace length; currently set at 8.
     pub const MIN_TRACE_LENGTH: usize = 8;
     /// Maximum number of columns in an execution trace (across all segments); currently set at 255.
-    pub const MAX_TRACE_WIDTH: usize = 255;
+    pub const MAX_TRACE_WIDTH: usize = 2047;
     /// Maximum number of bytes in trace metadata; currently set at 65535.
     pub const MAX_META_LENGTH: usize = 65535;
     /// Maximum number of random elements per auxiliary trace segment; currently set to 255.
